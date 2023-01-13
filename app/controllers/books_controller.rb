8db3e572e961078532ac77196590c8e8a4356504
EC2 Default User <ec2-user@ip-172-31-84-230.ec2.internal>
2023-01-13 14:55:41 +0000

    [Add]ValidationとFlashメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,15 @@ class BooksController < ApplicationController
   end
   
   def create
-    new_book = Book.new(book_params)
-    new_book.save
-    redirect_to book_path(id: new_book.id)
+    @book = Book.new(book_params)
     # redirect_to '/books'
+    if @book.save
+      flash[:success] = 'New book added successfully'
+      redirect_to book_path(id: @book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -17,19 +22,33 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @editing_book = Book.find(params[:id])
   end
   
   def update
-    editing_book = Book.find(params[:id])
-    editing_book.update(book_params)
-    redirect_to book_path(editing_book.id)  
+    @editing_book = Book.find(params[:id])
+    
+    if @editing_book.update(book_params)
+      flash[:success] = 'The book edited successfully'
+      redirect_to book_path(@editing_book.id) 
+    else
+      
+      render :edit
+    end
+    
+     
   end
   
   def destroy
     destroying_book = Book.find(params[:id])
-    destroying_book.destroy
-    redirect_to books_path
+    if destroying_book.destroy
+      flash[:success] = 'The book deleted successfully'
+      redirect_to books_path 
+    else
+      
+      render :index
+    end
+    
   end
   
   private
